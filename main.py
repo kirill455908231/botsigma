b24fd26cee20e5470b72b331ae5bb4c44051a794
kirill455908231 <kirill4559082@gmail.com>
2025-06-21 19:26:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
-#!/usr/bin/python
+# This example show how to use inline keyboards and process button presses
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# This is a simple bot with schedule timer
-# https://schedule.readthedocs.io
+TELEGRAM_TOKEN = '<TOKEN>'
 
-from Config import TOKEN
-import time, threading, schedule
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-import telebot 
-from telebot TOKEN
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
+                               InlineKeyboardButton("No", callback_data="cb_no"))
+    return markup
 
-bot = telebot(TOKEN)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "cb_yes":
+        bot.answer_callback_query(call.id, "Answer is Yes")
+    elif call.data == "cb_no":
+        bot.answer_callback_query(call.id, "Answer is No")
 
+@bot.message_handler(func=lambda message: True)
+def message_handler(message):
+    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
 
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, "Hi! Use /set <seconds> to set a timer")
-
-
-def beep(chat_id) -> None:
-    """Send the beep message."""
-    bot.send_message(chat_id, text='Beep!')
-
-
-@bot.message_handler(commands=['set'])
-def set_timer(message):
-    args = message.text.split()
-    if len(args) > 1 and args[1].isdigit():
-        sec = int(args[1])
-        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
-    else:
-        bot.reply_to(message, 'Usage: /set <seconds>')
-
-
-@bot.message_handler(commands=['unset'])
-def unset_timer(message):
-    schedule.clear(message.chat.id)
-
-
-if __name__ == '__main__':
-    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+bot.infinity_polling()
